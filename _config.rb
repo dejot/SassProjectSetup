e898c3e8e0c30c8a9e4071fc8f5a5cd87ee9fe56
darkojergovic <dajergovic@gmail.com>
2014-11-18 15:37:38 +0100

    Verzeichnisse hinzugefügt - alles neu!

---
+++ _config.rb
@@ -0,0 +1,13 @@
+require "susy"
+require "breakpoint"
+
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'images'
+fonts_dir = 'fonts'
+javascripts_dir = 'javascripts'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
